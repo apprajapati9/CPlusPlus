d7a00a54cb0dc86951a30e02ff4bef07734f8b29
apprajapati9 <apprajapati@hotmail.com>
2021-11-09 05:09:22 -0800

    p to pointer explanation exercise

--- C++/algorithms/Linkedlist/Pointers/double_pointer_use.cpp
+++ C++/algorithms/Linkedlist/Pointers/double_pointer_use.cpp
@@ -1,19 +1,30 @@
 #include <stdio.h>
+#include <iostream>
 
 /*
 Example to understand why we need a pointer to a pointer.
  
  */
 
+using namespace std; 
 
 void cant_change(int * x, int * z){
     x = z;
-    printf("\n ----> value of 'a' is: %x inside function, same as 'f', BUT will it be the same outside of this function? lets see\n", x);
+    cout << "cant_change() = Value of a inside cant_change function " << x << " and " << z << endl;
+    cout << "cant_change() = Inside can't change function change will stay the same outside of this function scope?" << endl;
+
+    cout << "can't change() = address of pointer a= " << &x << endl;
 }
 
 void change(int ** x, int * z){
-    *x = z;
-    printf("\n ----> value of 'a' is: %x inside function, same as 'f', BUT will it be the same outside of this function? lets see\n", *x);
+    *x = z; 
+    //dereferecing pointer to a pointer... **x = *x
+
+    cout << "Change() = a's value inside change() a= " << *x  << " but because of pointer to pointer, this change should be take effect in main() too." << endl;
+    cout << endl;
+    cout << "Change() = address of pointer a= " << &*x << endl; 
+    // &*x = address of pointer pointing to (*x) 
+
 }
 
 int main()
@@ -28,21 +39,67 @@ int main()
     int * f = &e;
     int ** pp = &a;  // pointer to pointer 'a'
 
-    printf("\n a's value: %x \n", a);
-    printf("\n b's value: %x \n", b);
-    printf("\n f's value: %x \n", f);
-    printf("\n can we change a?, lets see \n");
-    printf("\n a = b \n");
-    a = b;
-    printf("\n a's value is now: %x, same as 'b'... it seems we can, but can we do it in a function? lets see... \n", a);
-    printf("\n cant_change(a, f); \n");
-    cant_change(a, f);
-    printf("\n a's value is now: %x, Doh! same as 'b'...  that function tricked us. \n", a);
+    cout << "Main() = a' value= " << a << endl;
+    cout << "Main() = b' value= " << b << endl;
+    cout << "Main() = f' value= " << f << endl;
 
-    printf("\n NOW! lets see if a pointer to a pointer solution can help us... remember that 'pp' point to 'a' \n");
-     printf("\n change(pp, f); \n");
-    change(pp, f);
-    printf("\n a's value is now: %x, YEAH! same as 'f'...  that function ROCKS!!!. \n", a);
+    cout << endl;
+
+    cout << "Main() = a' address= " << &a << endl;
+    cout << "Main() = b' address= " << &b << endl;
+    cout << "Main() = f' address= " << &f << endl;
+    
+    cout << endl;
+    
+    cout << "Main() = c' address= " << &c << endl;
+    cout << "Main() = d' address= " << &d << endl;
+    cout << "Main() = e' address= " << &e << endl;
+
+    cout << endl;
+ 
+    cout << "Main() = Can we change a pointer? Let's see" << endl;
+    cout << endl;
+    cout << "Main() = operation performing a = b" << endl;
+    cout << endl;
+    a = b;
+    cout << "Main() = a' value after a = b is =" << a << " = Same as b's value"  << endl;
+    cout << endl;
+    cout << "Main() = a and b dereference, a= " << *a << " and b=" << *b << endl;
+    
+    cout << endl;
+    cout << "Main() = Can we perfom change in a function?" << endl;  
+    
+    cout << endl;
+    cout << "Main() = Calling cant_change(a, f) function." << endl;
+
+    cant_change(a, f);  // Passing pointers so can be regarded as *a, pass by value but for pointers
+    //can think of this as *a, so duplicate values are stored in function scope.
+
+    //lets print pointer's address so the picture can be clearer
+    cout << endl;
+    cout << "Main() = address of pointer a " << &a << endl; 
+    
+    cout << endl;
+    cout << "Main() = value of a outside of can't cange function scope is " << a << endl;
+    
+    cout << endl;
+    cout << "Main() = outside of cant_change function, a's value hasn't changed. it is same as a=b " << endl;
+
+    cout << endl;
+    cout << "Main() = Let's see if pointer to a pointer can help us with changing value outside of main() and still remain the same in Main() " << endl;
+
+    change (&a, f); // passsing address of a pointer, pass by reference if you will, but for pointer.
+    //can think of it as.... & of *a, so in function  change (int * (*a), *f);
+
+    cout << endl;
+    cout << "Main() = a value " << a << endl;
+
+    //you don't need to pass address if you have double pointer already
+    
+    //pp is **p = &a; --- so below are the same.
+    //change(pp, f);  is same as passing.... change(&a, f);
+    //hope that clears up. 
+
     return 0;
 }
 

727af0fc09d21f2be155a60b6603ddb676312128
Ajay P. Prajapati <apprajapati@hotmail.com>
2022-09-20 21:33:46 -0400

    quick sort example

---
+++ C++/algorithms/quick_sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int partition(int *A, int start, int end){
+
+    int pivot = A[end];
+    int partitionIndex = start;
+
+    for(int i= start; i < end; i++){
+        
+        if(A[i] <= pivot){
+            swap( A[i], A[partitionIndex]);
+            partitionIndex++;
+        }
+    }
+
+    swap(A[partitionIndex], A[end]);
+    return partitionIndex;
+}
+
+void quickSort(int *A, int start, int end){
+    if( start < end){
+
+        int partitionIndex= partition(A, start, end);
+        quickSort(A, start,partitionIndex - 1);
+        quickSort(A, partitionIndex+1, end);
+    }
+}
+
+
+int main(){
+    int A[] = {7, 2, 1, 6, 8, 5, 3, 4};
+
+    quickSort(A, 0, 7);
+
+    for(int i = 0; i < 8; i++) cout << A[i] <<" ";
+}

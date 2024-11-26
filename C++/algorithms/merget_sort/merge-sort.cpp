357d97d9bf3f2910fe4ba4dadd011966b8b12a69
apprajapati <apprajapati@hotmail.com>
2024-11-26 00:14:28 -0500

    more recursion practice by printing merge sort result in reverse

--- C++/algorithms/merget_sort/merge-sort.cpp
+++ C++/algorithms/merget_sort/merge-sort.cpp
@@ -7,56 +7,56 @@ void sort(int *arr, int start, int end, int mid)
 
     int left = start; //left half start
     int right = mid + 1; //right half start
-    
+
     int s1 = mid - start + 1; //Size of left size arr
     int s2 = end - right + 1; // size of right size arr
-    
+
     int mergedArr[s1+s2] = {0}; // mergedArr to combine left and right.
-    int index = 0; 
-    
-    while(s1 != 0 && s2 != 0){
+    int index = 0;
 
-	if(arr[left] < arr[right] || arr[left] == arr[right]){
-	    mergedArr[index] = arr[left];
-	    left++;
-	    index++;
-	    s1--;
-	}
+    while(s1 != 0 && s2 != 0) {
 
-	if(arr[right] < arr[left]){
-	    mergedArr[index] = arr[right];
-	    right++;
-	    index++;
-	    s2--;
-	}
+        if(arr[left] < arr[right] || arr[left] == arr[right]) {
+            mergedArr[index] = arr[left];
+            left++;
+            index++;
+            s1--;
+        }
+
+        if(arr[right] < arr[left]) {
+            mergedArr[index] = arr[right];
+            right++;
+            index++;
+            s2--;
+        }
     }
 
     // Here keep using left variable to access correct left element.
     if(s1 > 0) {
-	for(int i = 0; i < s1; i++){
-	    mergedArr[index]= arr[left];
-	    index++;
-	    left++; //increment. 
-	}
+        for(int i = 0; i < s1; i++) {
+            mergedArr[index]= arr[left];
+            index++;
+            left++; //increment.
+        }
     }
 
     //here keep using right variable to access correct right  element.
     if(s2 > 0) {
-	for(int i = 0; i < s2; i++){
-	    mergedArr[index]= arr[right];
-	    index++;
-	    right++;// increment.
-	}
+        for(int i = 0; i < s2; i++) {
+            mergedArr[index]= arr[right];
+            index++;
+            right++;// increment.
+        }
 
     }
 
     //updating original arr.
     index = 0;
-    for(int i = start; i <= end; i++){
-	arr[i] = mergedArr[index];
-	index++;
+    for(int i = start; i <= end; i++) {
+        arr[i] = mergedArr[index];
+        index++;
     }
-   
+
 }
 
 void mergeSort(int *arr, int start, int end, int size)
@@ -73,6 +73,15 @@ void mergeSort(int *arr, int start, int end, int size)
     }
 }
 
+//Just a practice to print in reverse using recursion.
+void printReverse(int *arr, int value, int size)
+{
+    if(value < size) {
+        printReverse(arr, value + 1, size);
+    }
+    cout << arr[value] << ",";
+}
+
 int main()
 {
 
@@ -80,7 +89,7 @@ int main()
 
     int size = sizeof(arr)/ sizeof(int);
 
-    cout << "Printing array:: ";
+    cout << "Printing array:  " << endl;
     for(int i = 0; i < size; i++) {
         cout << arr[i] << ",";
     }
@@ -92,8 +101,13 @@ int main()
     cout << "Sorted array:: " << endl;
     for(int i = 0; i < size; i++) {
         cout << arr[i] << ",";
+
     }
-   
+
+    cout << endl << endl;
+    cout << "Printing sorted array in reverse using Recursion: " << endl;
+    printReverse(arr, 0, size-1);
+
     cout << endl;
     return 0;
 }
